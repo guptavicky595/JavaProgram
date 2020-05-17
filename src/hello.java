ef9f035aae167d1dce144ba986c2e86e5ba2e5e4
guptavicky595 <guptavicky595@gmail.com>
2020-05-17 17:03:10 +0530

    Initial Commit

---
+++ src/hello.java
@@ -0,0 +1,12 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello-World");
+		System.out.println("Welcome Vicky Gupta to the world of Devops!!!!!!!!");
+		System.out.println("INDIA");
+
+	}
+
+}
